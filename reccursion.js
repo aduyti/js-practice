a9d1d8b2f55da4f738ea95d4526185efa4e62a6a
Anirudhya <anirudhyaduyti@gmail.com>
2022-02-07 02:19:27 +0600

    somr rec

--- reccursion.js
+++ reccursion.js
@@ -5,3 +5,34 @@ function compare(a, b) {
         return false;
     }
 }
+
+function additionToN(n) {
+    if (n == 0) {
+        return 0;
+    }
+    return n + additionN(--n);
+}
+
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    }
+    return n * factorial(--n);
+}
+
+function fibonacciRed(n) {
+    if (n == 1) {
+        return 0;
+    }
+    if (n == 2) {
+        return 1;
+    }
+    return fibonacciRed(n - 1) + fibonacciRed(n - 2);
+}
+
+function fibonacci(n) {
+    for (let i = 1; i <= n; i++) {
+        console.log(fibonacciRed(i));
+    }
+}
+fibonacci(9);
